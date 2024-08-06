1ae512b7a338c4938a89294cc3807c1d5b28eb07
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-08-07 00:35:50 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import NotesPage from "./components/NotesPage";
-import "./App.css"
+import "./App.css";
+import Landing2 from "./components/Landing2";
+import Narrow from "./components/Common/Narrow";
+import Navbar from "./components/Common/Navbar";
 
 const App = () => {
-
   const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
 
   return (
     <div className="App">
       {!isAuthenticated && (
-        <button onClick={(e) => loginWithRedirect()}>Login with Redirect</button>
+        <>
+          <button onClick={(e) => loginWithRedirect()}>Login with Redirect</button>
+          <Narrow>
+            <Navbar />
+            <Landing2 />
+          </Narrow>
+        </>
       )}
       {isAuthenticated && (
         <>
-          <NotesPage/>
+          <NotesPage />
         </>
       )}
     </div>
---
+++ src/components/Landing2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Landing2() {
+  return (
+    <div>
+      <div className='text-2xl'>Members Directory</div>
+
+    </div>
+  )
+}
+
+export default Landing2
