61ff3ff21163b5f80889b98ad0afbc3dcbec6f3f
Madddynaks <madhav.nakra2002@gmail.com>
2024-08-07 04:03:53 +0530

    Start from here button

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -17,7 +17,7 @@ function Landing() {
                <div className=' text-4xl w-2/3 font-light'>your simple, intuitive note-taking app.
                </div>
                <div className='m-3 w-2/3'>
-               <button className=' text-black text-3xl inline-flex items-center whitespace-nowrap select-none justify-center bg-white font-normal gap-2 rounded-3xl px-5 md:px-7 py-2 md:py-3' onClick={(e) => loginWithRedirect()}>Start from here</button>
+               <button className=' text-black text-3xl inline-flex items-center whitespace-nowrap select-none justify-center bg-white font-normal gap-2 rounded-3xl px-5 hover:px-8 hover:-translate-y-2 md:px-7 py-2 md:py-3  transition-all duration-500 ease-in-out' onClick={(e) => loginWithRedirect()}>Start from here</button>
                </div>
             </div>
         </div>
