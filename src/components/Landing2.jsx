54ba26c101838edca1ac6a47ba7e8cc09b790993
Drishti Aggarwal <97733511+DrishtiiAggarwal@users.noreply.github.com>
2024-08-07 16:46:31 +0530

    members-directory-and-footer-in-landingpage

--- src/components/Landing2.jsx
+++ src/components/Landing2.jsx
@@ -35,14 +35,47 @@ function Landing2() {
           </div>
         </Narrow>
       </div>
-      <div className="bg-[#1E1E1E] pt-10 overflow-hidden">
+      <div className="bg-[#1E1E1E]  overflow-hidden">
         <Narrow>
-          <div className="py-40">
-            <div className="text-6xl text-center text-white mb-10">
+          <div className="py-[160px]">
+            <div className="text-6xl text-center font-medium pb-8 mx:md:pb-4 text-white mb-10">
               Members Directory
             </div>
-            <div className="grid grid-cols-3  gap-10 max-md:grid-cols-2 max-sm:grid-cols-1 max-sm:p-20">
-              <div className="bg-[#FFD966] flex p-10 max-xl:px-4 max-lg:flex-col max-lg:items-center">
+            <div className="grid grid-cols-4  gap-10 max-md:grid-cols-2 max-sm:grid-cols-1 max-sm:p-20">
+            <div className="bg-[#80CAFF] flex p-10 max-xl:px-4 flex-col items-center">
+                <div className="bg-white p-3 w-full">
+                  {" "}
+                  <img
+                    src="https://ik.imagekit.io/ouhkc1hsep/New%20Folder/Devesh.png?updatedAt=1723024711755"
+                    alt=""
+                  />
+                </div>
+                <div className=" w-full text-center py-2 max-xl:ml-3 max-md:text-xs">
+                  <div>Devesh Singh</div>
+                  <div className="text-sm text-wrap py-1 max-md:text-xs ">
+                    <a href="mailto:email@gmail.com" target="blank" rel="noopener noreferrer">Email ID</a>
+                  </div>
+                  <div className="flex gap-3 justify-center py-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height={35}
+                      width={35}
+                      viewBox="0 0 496 512"
+                    >
+                      <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height={35}
+                      width={35}
+                      viewBox="0 0 448 512"
+                    >
+                      <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-[#FFAFA3] flex p-10 max-xl:px-4 flex-col items-center">
                 <div className="bg-white p-3 w-full">
                   {" "}
                   <img
@@ -50,10 +83,11 @@ function Landing2() {
                     alt=""
                   />
                 </div>
-                <div className=" ml-10 w-6/12 max-xl:ml-3">
+                <div className=" w-full text-center py-2 max-xl:ml-3 max-md:text-xs">
                   <div>Drishti Aggarwal</div>
-                  <div className="text-sm text-wrap">email@gmail.com</div>
-                  <div className="flex gap-3">
+                  <div className="text-sm text-wrap py-1 max-md:text-xs">
+                    <a href="mailto:drishtiaggarwasl17@gmail.com" target="blank" rel="noopener noreferrer">Email ID</a></div>
+                  <div className="flex gap-3 justify-center py-3">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       height={35}
@@ -64,8 +98,8 @@ function Landing2() {
                     </svg>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      height={40}
-                      width={40}
+                      height={35}
+                      width={35}
                       viewBox="0 0 448 512"
                     >
                       <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
@@ -73,17 +107,19 @@ function Landing2() {
                   </div>
                 </div>
               </div>
-              <div className="bg-[#FFAFA3] flex p-10  max-xl:px-4 max-lg:flex-col max-lg:items-center">
-                <div className="bg-white p-3 w-full h-full">
+              <div className="bg-[#DEB8FF] flex p-10  max-xl:px-4 flex-col items-center">
+                <div className="bg-white p-3 w-full">
                   <img
                     src="https://ik.imagekit.io/ouhkc1hsep/New%20Folder/WhatsApp%20Image%202024-07-27%20at%2017.56.25_bcaf4d0c.jpg?updatedAt=1722983948064"
                     alt=""
                   />
                 </div>
-                <div className=" ml-10 w-6/12 max-xl:ml-5">
+                <div className="  w-full text-center py-2 max-xl:ml-5 max-md:text-xs">
                   <div>Madhav Nakra</div>
-                  <div className="text-sm text-wrap">email@gmail.com</div>
-                  <div className="flex gap-3">
+                  <div className="text-sm text-wrap py-1 max-md:text-xs ">
+                    <a href="mailto:email@gmail.com" target="blank" rel="noopener noreferrer">Email ID</a>
+                  </div>
+                  <div className="flex gap-3 justify-center py-3">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       height={35}
@@ -94,8 +130,8 @@ function Landing2() {
                     </svg>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      height={40}
-                      width={40}
+                      height={35}
+                      width={35}
                       viewBox="0 0 448 512"
                     >
                       <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
@@ -103,12 +139,14 @@ function Landing2() {
                   </div>
                 </div>
               </div>
-              <div className="bg-[#85E0A3] flex p-10  max-xl:px-4 max-lg:flex-col max-lg:items-center">
-                <div className="bg-white p-3 w-5/12"> <img src="https://ik.imagekit.io/ouhkc1hsep/New%20Folder/Yadav2.jpg?updatedAt=1722984971272" alt="" /></div>
-                <div className=" ml-10 w-6/12 max-xl:ml-5">
+              <div className="bg-[#FFD966] flex p-10  max-xl:px-4 flex-col items-center">
+                <div className="bg-white p-3 w-full "> <img src="https://ik.imagekit.io/ouhkc1hsep/New%20Folder/yadav3.jpg?updatedAt=1723023727079" alt="" /></div>
+                <div className=" w-full text-center py-2 max-xl:ml-5 max-md:text-xs">
                   <div>Shubham Yadav</div>
-                  <div className="text-sm text-wrap">email@gmail.com</div>
-                  <div className="flex gap-3">
+                  <div className="text-sm text-wrap py-1 max-md:text-xs">
+                    <a href="mailto:email@gmail.com" target="blank" rel="noopener noreferrer">Email ID</a>
+                  </div>
+                  <div className="flex gap-3 justify-center py-3">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       height={35}
@@ -119,8 +157,8 @@ function Landing2() {
                     </svg>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      height={40}
-                      width={40}
+                      height={35}
+                      width={35}
                       viewBox="0 0 448 512"
                     >
                       <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
@@ -131,7 +169,7 @@ function Landing2() {
             </div>
           </div>
         </Narrow>
-        <div className="mt-72 text-6xl relative -top-20 text-[#FFFFFF] opacity-70 text-center scale-[750%] font-semibold max-2xl:scale-[700%] max-xl:scale-[600%] max-lg:scale-[490%] max-md:scale-[375%] max-sm:scale-[250%] ">
+        <div className="mt-96 text-6xl relative -top-20 text-[#FFFFFF] opacity-70 text-center scale-[750%] font-semibold max-2xl:scale-[700%] max-xl:scale-[600%] max-lg:scale-[490%] max-md:scale-[375%] max-sm:scale-[250%] ">
           noteify
         </div>
       </div>
