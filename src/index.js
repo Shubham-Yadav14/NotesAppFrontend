d5ded430afac357feccc468cbd5722aa3f71ca7c
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-08-07 22:41:02 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
     domain="dev-0o6fpuzsivlewh2d.us.auth0.com"
-    clientId="gCoa77PyZqdmsWnSODrxNVWIwQNRoCke"
+    clientId="Jgyc0qnZ2L2ohYIW3cJSDzTBKsgqNEE5"
     authorizationParams={{
       redirect_uri: window.location.origin
     }}>
